a0016eb22ed35c54f7f0da742fc542ddb703a4cf
Girish Krishna <82050669+GirishKrishna25@users.noreply.github.com>
2022-10-20 20:17:20 +0530

    Update nextPermutation.java

--- Arrays/nextPermutation.java
+++ Arrays/nextPermutation.java
@@ -10,6 +10,7 @@ public class Main
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 4, 2};
         // int[] arr = {5, 4, 3, 2, 1};
+        // this gives the [1, 2, 3, 4, 5] as answer as the next permutation
         
         // use this line in assessemnts
         // if (arr == null || arr.length <= 1 ) return;
